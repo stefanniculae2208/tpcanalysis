cf93e418afaf441548bf5a44c5fd0905cd918b8b
stefanniculae2208 <stefan.niculae2208@gmail.com>
2022-10-07 11:24:01 +0300

    my commit

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
 git commit -m "my commit"
-git push https://github.com/stefanniculae2208/DELILA_TQDC.git main
+git push https://github.com/stefanniculae2208/tpcanalysis.git main
