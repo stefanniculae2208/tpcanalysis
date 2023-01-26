7bf2e94eb0bd4e43db29b243e2aa38e26a06cdf2
stefanniculae2208 <stefan.niculae2208@gmail.com>
2023-01-26 13:15:27 +0200

    my commit

--- test.cpp
+++ test.cpp
@@ -1599,7 +1599,7 @@ void test_convert_to_csv(int entry_nr = 210)
 
 
 
-void view_data_entries()
+void view_raw_data()
 {
 
 
@@ -1634,15 +1634,6 @@ void view_data_entries()
     
 
 
-    convertHitData loc_convert_hit;
-
-    
-
-
-
-    convertXYZ loc_conv_xyz;
-
-
     
 
 
@@ -1663,7 +1654,7 @@ void view_data_entries()
 
     auto loc_canv = new TCanvas("View entries canvas", "View entries", 1500, 1000);
     auto loc_pad = new TPad("pad name", "pad title", 0,0,1,1);
-    loc_pad->Divide(3, 3);
+    loc_pad->Divide(3, 1);
     loc_pad->Draw();
     
     TH2D *u_hists = nullptr;
@@ -1671,17 +1662,6 @@ void view_data_entries()
     TH2D *w_hists = nullptr;
 
 
-    TGraph *u_graph = nullptr;
-    //TAxis *u_axis = nullptr;
-    TGraph *v_graph = nullptr;
-    //TAxis *v_axis = nullptr;
-    TGraph *w_graph = nullptr;
-    //TAxis *w_axis = nullptr;
-
-
-    TGraph *p_graph = nullptr;
-    TAxis *p_axis = nullptr;
-    TGraph2D *p_graph3d = nullptr;
 
 
 
@@ -1786,54 +1766,11 @@ void view_data_entries()
 
 
 
-        err = loc_conv_uvw.substractBl();
-
-        if(err != 0)
-            std::cout<<"Substractbl error code "<<err<<std::endl;
 
 
         data_container.uvw_data = loc_conv_uvw.returnDataUVW();
 
         std::cout<<"UVW data size is "<<data_container.uvw_data.size()<<std::endl;
-
-
-        err = loc_convert_hit.setUVWData(data_container.uvw_data);
-        if(err != 0)
-            std::cout<<"Error set UVW data code "<<err<<std::endl;
-
-        err = loc_convert_hit.getHitInfo();
-        if(err != 0)
-            std::cout<<"Error get hit info code "<<err<<std::endl;
-
-        data_container.hit_data = loc_convert_hit.returnHitData();
-        data_container.raw_hist_container = loc_convert_hit.returnHistData();
-
-        std::cout<<"Hit data size "<<data_container.hit_data.size()<<std::endl;
-        std::cout<<"Hist data size "<<data_container.raw_hist_container.size()<<std::endl;
-
-
-        err = loc_conv_xyz.getNewVector(data_container.hit_data);
-        if(err != 0)
-            std::cout<<"Error get new vector code "<<err<<std::endl;
-
-        err = loc_conv_xyz.makeConversionXYZ();
-        if(err != 0)
-            std::cout<<"Error make conversion XYZ code "<<err<<std::endl;
-
-
-        data_container.xyz_data = loc_conv_xyz.returnXYZ();
-
-        std::cout<<"XYZ vector size "<<data_container.xyz_data.size()<<std::endl;
-
-
-
-
-
-
-
-
-
-
 
 
         if(u_hists != nullptr)
@@ -1846,33 +1783,6 @@ void view_data_entries()
             w_hists->Reset();
 
 
-
-        if(u_graph != nullptr)
-            u_graph->Delete();
-
-        if(v_graph != nullptr)
-            v_graph->Delete();
-
-        if(w_graph != nullptr)
-            w_graph->Delete();
-
-        /* if(u_axis != nullptr)
-            u_axis->Delete();
-
-        if(v_axis != nullptr)
-            v_axis->Delete();
-
-        if(w_axis != nullptr)
-            w_axis->Delete(); */
-
-
-
-
-        if(p_graph != nullptr)
-            p_graph->Delete();
-
-        if(p_graph3d != nullptr)
-            p_graph3d->Delete();
 
         
 
@@ -1940,178 +1850,6 @@ void view_data_entries()
         loc_pad->cd(1); u_hists->Draw("COLZ");
         loc_pad->cd(2); v_hists->Draw("COLZ");
         loc_pad->cd(3); w_hists->Draw("COLZ");
-
-
-
-
-        std::vector<double> x_u;
-        std::vector<double> y_u;
-
-        std::vector<double> x_v;
-        std::vector<double> y_v;
-
-        std::vector<double> x_w;
-        std::vector<double> y_w;
-
-        
-
-        for(auto hit_iter : data_container.hit_data){
-
-            if(hit_iter.plane == 0){
-
-                x_u.push_back(hit_iter.peak_x);
-                y_u.push_back(hit_iter.strip);
-
-
-            }else if(hit_iter.plane == 1){
-
-                x_v.push_back(hit_iter.peak_x);
-                y_v.push_back(hit_iter.strip);
-
-            }else if(hit_iter.plane == 2){
-
-                x_w.push_back(hit_iter.peak_x);
-                y_w.push_back(hit_iter.strip);
-
-            }
-
-        }
-
-
-
-
-
-
-
-
-        
-
-
-        
-
-        
-        if(data_container.hit_data.size() != 0){
-
-        
-
-
-            u_graph = new TGraph(x_u.size(), x_u.data(), y_u.data());
-            u_graph->GetXaxis()->SetLimits(-1, 512);
-            u_graph->GetHistogram()->SetMaximum(73);
-            u_graph->GetHistogram()->SetMinimum(0);
-            u_graph->SetMarkerColor(kBlack);
-            u_graph->SetMarkerStyle(kFullCircle);
-            u_graph->SetTitle("Hits detected on U plane; Time; Strip");
-            loc_pad->cd(4);u_graph->Draw("AP");
-            
-
-            v_graph = new TGraph(x_v.size(), x_v.data(), y_v.data());
-            v_graph->GetXaxis()->SetLimits(-1, 512);
-            v_graph->GetHistogram()->SetMaximum(93);
-            v_graph->GetHistogram()->SetMinimum(0);
-            v_graph->SetMarkerColor(kBlack);
-            v_graph->SetMarkerStyle(kFullCircle);
-            v_graph->SetTitle("Hits detected on V plane; Time; Strip");
-            loc_pad->cd(5);v_graph->Draw("AP");
-            
-
-            w_graph = new TGraph(x_w.size(), x_w.data(), y_w.data());
-            w_graph->GetXaxis()->SetLimits(-1, 512);
-            w_graph->GetHistogram()->SetMaximum(93);
-            w_graph->GetHistogram()->SetMinimum(0);
-            w_graph->SetMarkerColor(kBlack);
-            w_graph->SetMarkerStyle(kFullCircle);
-            w_graph->SetTitle("Hits detected on W plane; Time; Strip");
-            loc_pad->cd(6);w_graph->Draw("AP");
-            loc_pad->Modified();
-            loc_pad->Update();
-
-        }else{
-            u_graph = nullptr;
-            v_graph = nullptr;
-            w_graph = nullptr;
-        }
-
-
-        if(data_container.xyz_data.size() != 0){
-
-            std::vector<double> x, y, z;
-
-            x.push_back(-10.0);
-            y.push_back(-10.0);
-            z.push_back(-10.0);
-    
-
-
-            for(auto point_iter : data_container.xyz_data){
-
-                x.push_back(point_iter.data_x);
-                y.push_back(point_iter.data_y);
-                z.push_back(point_iter.data_z);
-
-        
-
-            }
-
-            x.push_back(150.0);
-            y.push_back(150.0);
-            z.push_back(150.0);
-
-
-
-
-            
-            p_graph = new TGraph(x.size(), x.data(), y.data());
-            p_graph->GetXaxis()->SetLimits(-10, 150);
-            p_graph->GetHistogram()->SetMaximum(150);
-            p_graph->GetHistogram()->SetMinimum(-10);
-            p_graph->SetMarkerColor(kBlack);
-            p_graph->SetMarkerStyle(kFullCircle);
-            p_graph->SetTitle("XY coordinates projection; X axis; Y axis");
-            loc_pad->cd(7);p_graph->Draw("AP");
-            loc_pad->Modified();
-            loc_pad->Update();
-
-            
-
-
-
-            p_graph3d = new TGraph2D(x.size(), x.data(), y.data(), z.data());
-            p_graph3d->SetMarkerColor(kBlue);
-            p_graph3d->SetMarkerStyle(kFullCircle);
-            p_graph3d->SetTitle("Reconstructed data in XYZ coordinates; X axis; Y axis; Z axis");
-            loc_pad->cd(8); p_graph3d->Draw("P0");
-            loc_pad->Modified();
-            loc_pad->Update();
-
-
-
-        }else{
-            p_graph = nullptr;
-            p_graph3d = nullptr;
-        }
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -3042,6 +2780,547 @@ void drawXYimage(int entry_nr)
 
 
 
+void view_data_entries()
+{
+
+
+
+
+
+    int entry_nr = -1;
+    int max_entries;
+
+
+
+
+    auto goodFile = "./rootdata/data2.root";
+
+    auto goodTree = "tree";
+
+
+    loadData good_data(goodFile, goodTree);
+
+    auto returned_file = good_data.openFile();
+    auto err = good_data.readData();
+    max_entries = good_data.returnNEntries();
+
+
+    convertUVW loc_conv_uvw;
+
+    err = loc_conv_uvw.openSpecFile();
+
+    if(err != 0)
+        return;
+
+    
+
+
+    convertHitData loc_convert_hit;
+
+    
+
+
+
+    convertXYZ loc_conv_xyz;
+
+
+    
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    auto loc_canv = new TCanvas("View entries canvas", "View entries", 1500, 1000);
+    auto loc_pad = new TPad("pad name", "pad title", 0,0,1,1);
+    loc_pad->Divide(3, 3);
+    loc_pad->Draw();
+    
+    TH2D *u_hists = nullptr;
+    TH2D *v_hists = nullptr;
+    TH2D *w_hists = nullptr;
+
+
+    TGraph *u_graph = nullptr;
+    //TAxis *u_axis = nullptr;
+    TGraph *v_graph = nullptr;
+    //TAxis *v_axis = nullptr;
+    TGraph *w_graph = nullptr;
+    //TAxis *w_axis = nullptr;
+
+
+    TGraph *p_graph = nullptr;
+    TAxis *p_axis = nullptr;
+    TGraph2D *p_graph3d = nullptr;
+
+
+
+
+    
+
+
+
+    
+
+    uint32_t loop_nr = 0;
+
+
+    while(1){
+
+        generalDataStorage data_container;
+        
+
+        std::cout<<"Press 'e' to exit, 'd' for next and 'a' for previous and i to input the entry number. Loop "<<loop_nr<<std::endl;
+
+        loop_nr++;
+
+        char key_val = getchar();
+
+        // Clear the input buffer
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        if(key_val == 'e'){
+
+            break;
+
+        }else if(key_val == 'd'){
+
+            if(entry_nr < max_entries){
+                entry_nr++;
+            }
+
+            std::cout<<"Entry number is "<<entry_nr<<"\n\n"<<std::endl;
+
+        }else if(key_val == 'a'){
+
+            if(entry_nr > 0){
+                entry_nr--;
+            }
+
+            std::cout<<"Entry number is "<<entry_nr<<"\n\n"<<std::endl;
+
+        }else if(key_val == 'i'){
+
+            std::cout<<"Please enter the entry number.\n";
+
+            std::cin>>entry_nr;
+
+            if(entry_nr < 0 || entry_nr > (max_entries - 1))
+                entry_nr = 0;
+
+
+            std::cout<<"Entry number is "<<entry_nr<<"\n\n"<<std::endl;
+
+        }
+
+
+
+
+        if(entry_nr < 0){
+            entry_nr = 0;
+        }
+
+
+
+        
+
+
+
+
+
+
+        err = good_data.decodeData(entry_nr);
+        if(err != 0){
+            std::cout<<"Error decode data code "<<err<<std::endl;
+        }
+
+        data_container.root_raw_data = good_data.returnRawData();
+
+        std::cout<<"RAW data size is "<<data_container.root_raw_data.size()<<std::endl;
+
+
+
+
+
+
+
+
+        
+        loc_conv_uvw.setRawData(data_container.root_raw_data);
+
+        
+
+        err = loc_conv_uvw.makeConversion();
+        if(err != 0)
+            std::cout<<"Make conversion error code "<<err<<std::endl;
+
+
+
+        err = loc_conv_uvw.substractBl();
+
+        if(err != 0)
+            std::cout<<"Substractbl error code "<<err<<std::endl;
+
+
+        data_container.uvw_data = loc_conv_uvw.returnDataUVW();
+
+        std::cout<<"UVW data size is "<<data_container.uvw_data.size()<<std::endl;
+
+
+        err = loc_convert_hit.setUVWData(data_container.uvw_data);
+        if(err != 0)
+            std::cout<<"Error set UVW data code "<<err<<std::endl;
+
+        err = loc_convert_hit.getHitInfo();
+        if(err != 0)
+            std::cout<<"Error get hit info code "<<err<<std::endl;
+
+        data_container.hit_data = loc_convert_hit.returnHitData();
+        data_container.raw_hist_container = loc_convert_hit.returnHistData();
+
+        std::cout<<"Hit data size "<<data_container.hit_data.size()<<std::endl;
+        std::cout<<"Hist data size "<<data_container.raw_hist_container.size()<<std::endl;
+
+
+        err = loc_conv_xyz.getNewVector(data_container.hit_data);
+        if(err != 0)
+            std::cout<<"Error get new vector code "<<err<<std::endl;
+
+        err = loc_conv_xyz.makeConversionXYZ();
+        if(err != 0)
+            std::cout<<"Error make conversion XYZ code "<<err<<std::endl;
+
+
+        data_container.xyz_data = loc_conv_xyz.returnXYZ();
+
+        std::cout<<"XYZ vector size "<<data_container.xyz_data.size()<<std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
+        if(u_hists != nullptr)
+            u_hists->Reset();
+
+        if(v_hists != nullptr)
+            v_hists->Reset();
+
+        if(w_hists != nullptr)
+            w_hists->Reset();
+
+
+
+        if(u_graph != nullptr)
+            u_graph->Delete();
+
+        if(v_graph != nullptr)
+            v_graph->Delete();
+
+        if(w_graph != nullptr)
+            w_graph->Delete();
+
+        /* if(u_axis != nullptr)
+            u_axis->Delete();
+
+        if(v_axis != nullptr)
+            v_axis->Delete();
+
+        if(w_axis != nullptr)
+            w_axis->Delete(); */
+
+
+
+
+        if(p_graph != nullptr)
+            p_graph->Delete();
+
+        if(p_graph3d != nullptr)
+            p_graph3d->Delete();
+
+        
+
+
+
+
+
+
+
+
+        u_hists = new TH2D(Form("u_hists_%d", entry_nr), Form("Histogram for U entry %d", entry_nr), 512, 1, 513, 100, 1, 101);
+        v_hists = new TH2D(Form("v_hists_%d", entry_nr), Form("Histogram for V entry %d", entry_nr), 512, 1, 513, 100, 1, 101);
+        w_hists = new TH2D(Form("w_hists_%d", entry_nr), Form("Histogram for W entry %d", entry_nr), 512, 1, 513, 100, 1, 101);
+
+
+
+
+
+
+        int bin = 0;
+        int strip = 1;
+
+        for(auto iter : data_container.uvw_data){
+
+            bin = 0;
+
+
+
+
+            if(iter.plane_val == 0){
+
+                for(auto sig_iter : iter.signal_val){
+
+                    u_hists->SetBinContent(++bin, iter.strip_nr, sig_iter);
+
+                }
+
+            }
+
+
+            if(iter.plane_val == 1){
+
+                for(auto sig_iter : iter.signal_val){
+
+                    v_hists->SetBinContent(++bin, iter.strip_nr, sig_iter);
+
+                }
+
+            }
+
+
+            if(iter.plane_val == 2){
+
+                for(auto sig_iter : iter.signal_val){
+
+                    w_hists->SetBinContent(++bin, iter.strip_nr, sig_iter);
+
+                }
+
+            }
+
+
+        }
+
+        loc_pad->cd(1); u_hists->Draw("COLZ");
+        loc_pad->cd(2); v_hists->Draw("COLZ");
+        loc_pad->cd(3); w_hists->Draw("COLZ");
+
+
+
+
+        std::vector<double> x_u;
+        std::vector<double> y_u;
+
+        std::vector<double> x_v;
+        std::vector<double> y_v;
+
+        std::vector<double> x_w;
+        std::vector<double> y_w;
+
+        
+
+        for(auto hit_iter : data_container.hit_data){
+
+            if(hit_iter.plane == 0){
+
+                x_u.push_back(hit_iter.peak_x);
+                y_u.push_back(hit_iter.strip);
+
+
+            }else if(hit_iter.plane == 1){
+
+                x_v.push_back(hit_iter.peak_x);
+                y_v.push_back(hit_iter.strip);
+
+            }else if(hit_iter.plane == 2){
+
+                x_w.push_back(hit_iter.peak_x);
+                y_w.push_back(hit_iter.strip);
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+        
+
+
+        
+
+        
+        if(data_container.hit_data.size() != 0){
+
+        
+
+
+            u_graph = new TGraph(x_u.size(), x_u.data(), y_u.data());
+            u_graph->GetXaxis()->SetLimits(-1, 512);
+            u_graph->GetHistogram()->SetMaximum(73);
+            u_graph->GetHistogram()->SetMinimum(0);
+            u_graph->SetMarkerColor(kBlack);
+            u_graph->SetMarkerStyle(kFullCircle);
+            u_graph->SetTitle("Hits detected on U plane; Time; Strip");
+            loc_pad->cd(4);u_graph->Draw("AP");
+            
+
+            v_graph = new TGraph(x_v.size(), x_v.data(), y_v.data());
+            v_graph->GetXaxis()->SetLimits(-1, 512);
+            v_graph->GetHistogram()->SetMaximum(93);
+            v_graph->GetHistogram()->SetMinimum(0);
+            v_graph->SetMarkerColor(kBlack);
+            v_graph->SetMarkerStyle(kFullCircle);
+            v_graph->SetTitle("Hits detected on V plane; Time; Strip");
+            loc_pad->cd(5);v_graph->Draw("AP");
+            
+
+            w_graph = new TGraph(x_w.size(), x_w.data(), y_w.data());
+            w_graph->GetXaxis()->SetLimits(-1, 512);
+            w_graph->GetHistogram()->SetMaximum(93);
+            w_graph->GetHistogram()->SetMinimum(0);
+            w_graph->SetMarkerColor(kBlack);
+            w_graph->SetMarkerStyle(kFullCircle);
+            w_graph->SetTitle("Hits detected on W plane; Time; Strip");
+            loc_pad->cd(6);w_graph->Draw("AP");
+            loc_pad->Modified();
+            loc_pad->Update();
+
+        }else{
+            u_graph = nullptr;
+            v_graph = nullptr;
+            w_graph = nullptr;
+        }
+
+
+        if(data_container.xyz_data.size() != 0){
+
+            std::vector<double> x, y, z;
+
+            x.push_back(-10.0);
+            y.push_back(-10.0);
+            z.push_back(-10.0);
+    
+
+
+            for(auto point_iter : data_container.xyz_data){
+
+                x.push_back(point_iter.data_x);
+                y.push_back(point_iter.data_y);
+                z.push_back(point_iter.data_z);
+
+        
+
+            }
+
+            x.push_back(150.0);
+            y.push_back(150.0);
+            z.push_back(150.0);
+
+
+
+
+            
+            p_graph = new TGraph(x.size(), x.data(), y.data());
+            p_graph->GetXaxis()->SetLimits(-10, 150);
+            p_graph->GetHistogram()->SetMaximum(150);
+            p_graph->GetHistogram()->SetMinimum(-10);
+            p_graph->SetMarkerColor(kBlack);
+            p_graph->SetMarkerStyle(kFullCircle);
+            p_graph->SetTitle("XY coordinates projection; X axis; Y axis");
+            loc_pad->cd(7);p_graph->Draw("AP");
+            loc_pad->Modified();
+            loc_pad->Update();
+
+            
+
+
+
+            p_graph3d = new TGraph2D(x.size(), x.data(), y.data(), z.data());
+            p_graph3d->SetMarkerColor(kBlue);
+            p_graph3d->SetMarkerStyle(kFullCircle);
+            p_graph3d->SetTitle("Reconstructed data in XYZ coordinates; X axis; Y axis; Z axis");
+            loc_pad->cd(8); p_graph3d->Draw("P0");
+            loc_pad->Modified();
+            loc_pad->Update();
+
+
+
+        }else{
+            p_graph = nullptr;
+            p_graph3d = nullptr;
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        loc_canv->Update();
+
+
+
+
+
+    }
+
+
+
+
+
+
+    loc_canv->WaitPrimitive();
+
+
+
+
+
+
+}
+
 
 
 void test()
@@ -3070,6 +3349,8 @@ void test()
 
     //view_data_entries();
 
+    view_raw_data();
+
     //create_entries_pdf("/media/gant/Expansion/tpcanalcsv/data07.root", "/media/gant/Expansion/tpcanalcsv/data07.pdf");
 
     //writeXYZcvs(429);
@@ -3077,7 +3358,7 @@ void test()
     //drawXYimage(429);
 
 
-    writeFullCSV();
+    //writeFullCSV();
 
     
 
